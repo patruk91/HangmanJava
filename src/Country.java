087a4172bdbb3a56fc7e1f8ff8696194e6f9ae98
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-27 21:05:41 +0100

    Add Country class, to get information aboub countries and capitals

---
+++ src/Country.java
@@ -0,0 +1,13 @@
+public class Country {
+    private String name;
+    private String capital;
+
+    public Country(String[] name) {
+
+    }
+
+    public void add(String name, String capital) {
+        this.name = name;
+        this.capital = capital;
+    }
+}
