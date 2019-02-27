9b40c9073db6f154f2f125542ccc90d61b359fa4
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-27 21:04:56 +0100

    Read data from file and send it as object to Country class

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,27 +1,41 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class DataManager {
 
 
-    private void readFile(File fileObject)  {
+    private ArrayList<Country> readFile(File fileObject)  {
         Scanner reader;
+        ArrayList<Country> dataList = new ArrayList<>();
 
         try {
             reader = new Scanner(fileObject);
         } catch (Exception error) {
             System.out.println("We couldn't read the file. Error: " + error.getMessage());
-            return;
+            return dataList;
         }
 
         while (reader.hasNextLine()) {
-            String line = reader.nextLine();
-            System.out.println(line);
+            String[] countryAndCapital = reader.nextLine().split("\\|");
+            System.out.println(Arrays.toString(countryAndCapital));
+            dataList.add(new Country(countryAndCapital));
+//            System.out.println(dataList);
         }
         reader.close();
+        return dataList;
     }
-    
+
+    public static void main(String[] args) {
+        File fileName = new File("countries_and_capitals.txt");
+
+        DataManager dataManager = new DataManager();
+
+
+        System.out.println(dataManager.readFile(fileName));
+
+    }
 
 }
