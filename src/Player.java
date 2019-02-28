9ea1fd86d8b59ad9cbb0ad6d456af6a8b73181b6
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-28 09:09:17 +0100

    Remove methond get user name, add new for not guessed words

--- src/Player.java
+++ src/Player.java
@@ -1,21 +1,17 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Player {
     private String name;
     private int guessTries;
     private int guessLives;
+    private ArrayList<String> incorrectLetterOrWords = new ArrayList<>();
 
 
     public Player(String name, int guessTries, int guessLives) {
         this.name = name;
         this.guessTries = guessTries;
         this.guessLives = guessLives;
-    }
-
-    public String askForUserName() {
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Input your name: ");
-        return reader.nextLine();
     }
 
     public String getName() {
@@ -45,4 +41,11 @@ public class Player {
     public long endTime() {
         return System.currentTimeMillis();
     }
+
+    public ArrayList<String> badLettersOrWords(String letterOrWord) {
+        incorrectLetterOrWords.add(letterOrWord);
+        return incorrectLetterOrWords;
+    }
+
+
 }
