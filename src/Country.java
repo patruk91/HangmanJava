286a0d99a5732ab33ee94ac4853a0f01fa5da0da
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-28 07:23:21 +0100

    Add getters, and toString function

--- src/Country.java
+++ src/Country.java
@@ -1,8 +1,25 @@
+import java.lang.reflect.Array;
+
 public class Country {
     private String name;
     private String capital;
 
     public Country(String[] countryAndCapital) {
+        this.name = countryAndCapital[0];
+        this.capital = countryAndCapital[1];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + capital + "\n";
     }
 
 }
