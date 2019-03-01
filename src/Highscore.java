252af872f753b008ac5a669e3fe647027a742c1b
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-01 09:00:07 +0100

    Change filename

---
+++ src/Highscore.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HangmanLogic {
+    public static void main(String[] args) {
+        DataManager dataManager = new DataManager();
+
+        System.out.println(dataManager.readFile(new File("highscore.txt")));
+        ArrayList<String> totalHighscore = new ArrayList<>();
+        totalHighscore = dataManager.readFile(new File("highscore.txt"));
+
+        ArrayList<String> results = new ArrayList<>();
+
+        Integer[] columnsWidth = {0, 0, 0, 0, 0};
+
+        for (String line : totalHighscore) {
+            String[] record = line.split(";");
+
+            for (int i = 0; i < record.length; i++) {
+                if (record[i].length() > columnsWidth[i]) {
+                    columnsWidth[i] = record[i].length();
+                }
+            }
+        }
+        System.out.println(Arrays.toString(columnsWidth));
+
+
+        //dynamic printing
+        String format ="";
+
+        for (int x:columnsWidth)
+            format += "%-" + (x + 3) + "s ";
+
+        format += "%n";
+
+        for (String line : totalHighscore) {
+            String[] record = line.split(";");
+
+                System.out.printf(format, record);
+            }
+
+    }
+}
+
+
