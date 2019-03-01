91aec36417815952c113c535aa2c357bde15d10b
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-01 10:29:24 +0100

    Add new Score object to sort data in text file

--- src/Highscore.java
+++ src/Highscore.java
@@ -1,40 +1,39 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Highscore {
+
+
     public static void main(String[] args) {
         DataManager dataManager = new DataManager();
 
-        System.out.println(dataManager.readFile(new File("highscore.txt")));
-        ArrayList<String> totalHighscore = new ArrayList<>();
+//        System.out.println(dataManager.readFile(new File("highscore.txt")));
+        ArrayList<String> totalHighscore;
         totalHighscore = dataManager.readFile(new File("highscore.txt"));
-
-        ArrayList<String> results = new ArrayList<>();
-
+        ArrayList<Score> sortedhighscore = new ArrayList<>();
 
 
+        for (String line : totalHighscore) {
+            String[] record = line.split(";");
+            sortedhighscore.add(new Score(record));
+        }
+
+//        System.out.println(sortedhighscore.get(0).getTries());
+//        System.out.println(Arrays.asList(sortedhighscore.toArray()));
+//        sortedhighscore.sort(Comparator.comparing(Score::getTries));
+//        System.out.println(sortedhighscore);
+//
+//        System.out.println();
+//
+//        for (Score item : sortedhighscore) {
+//            System.out.println(item.toString());
+//        }
+
+        System.out.println(sortedhighscore.toString());
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("\n\n\n");
         // width colums
         Integer[] columnsWidth = {0, 0, 0, 0, 0};
         for (String line : totalHighscore) {
@@ -46,9 +45,12 @@ public class Highscore {
                 }
             }
         }
+
+
+
+
+
         System.out.println(Arrays.toString(columnsWidth));
-
-
         //dynamic printing
         String format ="";
 
@@ -62,6 +64,7 @@ public class Highscore {
 
                 System.out.printf(format, record);
             }
+
 
     }
 }
---
+++ src/Score.java
@@ -0,0 +1,43 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Score {
+    private String name;
+    private String date;
+    private String time;
+    private String tries;
+    private String town;
+    private String[] score;
+
+    public Score(String[] score) {
+        this.name = score[0];
+        this.date = score[1];
+        this.time = score[2];
+        this.tries = score[3];
+        this.town = score[4];
+        this.score = score;
+
+    }
+
+    public int getTries() {
+        return Integer.parseInt(tries);
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println(Arrays.asList(score));
+        return String.format("%s %s %s %s %s",name,date,time,tries,town);
+    }
+}
+
+
+
+
